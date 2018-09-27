51fadef39f562ff8b8670844de5e6a786ca0e897
darlok77 <g.dalleau77@hotmail.com>
2018-09-27 19:33:12 +0200

    first commit

---
+++ src/components/news/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { getEventsData } from './actions'
+
+import Results from './components/results/index.js'
+
+class News extends Component {
+  componentDidMount() {
+    getEventsData()
+  }
+
+  render() {
+    const { news } = this.props
+
+    return (
+      <div>
+        <Results data={news.data} />
+      </div>
+    )
+  }
+}
+
+export default connect(state => state)(News)
